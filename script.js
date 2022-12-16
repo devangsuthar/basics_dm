78336f8633eaf15bf953623acd36e2a4fa9ec6c4
devang <devangsuthar24@gmail.com>
2022-12-16 23:55:39 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+    let tab_bg = $("[class=tab-list]");
+    tab_bg.css('background-color','#CCCCCC');    
+    
+    $('.tab-list li:last').css('border-right','none');
+});
